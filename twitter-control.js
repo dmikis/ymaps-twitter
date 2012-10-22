bfef3d194497db60a2a883612c013ae9136cc25f
Kirill Dmitrenko <kdmitrenko@gmail.com>
2012-10-23 02:01:06 +0400

    TweetsDownloadScheduler: first revision

--- twitter-control.js
+++ twitter-control.js
@@ -97,6 +97,84 @@ ymaps.ready(function () {
             }
         };
 
+    var TweetsDownloadScheduler = function (options) {
+        this.events = new ymaps.event.Manager({
+            controllers: [{
+                onStartListening: ymaps.util.bind(this._start, this),
+                onStopListening: ymaps.util.bind(this._stop, this)
+            }],
+            context: this
+        });
+        this.options = new ymaps.option.Manager(ymaps.util.extend({
+            updateInterval: 5000,
+            minUpdateInterval: 2500,
+            loadTweetsOnStart: true,
+            loadTweetsOnOptionsChanges: true
+        }, options));
+    };
+
+    ymaps.util.extend(TweetsDownloadScheduler.prototype, {
+
+        _start: function (eventManager, eventType) {
+            console.log('scheduler start');
+            if (eventType === 'tweetsloaded') {
+                this._timer = setInterval(
+                    ymaps.util.bind(this._tick, this),
+                    this.options.get('updateInterval'),
+                    true
+                );
+                if (this.options.get('loadTweetsOnStart')) {
+                    this._tick();
+                }
+                this.options.events.add('change', this._onOptionsChange, this);
+            }
+        },
+
+        _stop: function (eventManager, eventType) {
+            if (eventType === 'tweetsloaded') {
+                clearInterval(this._timer);
+                this._timer = null;
+                this.options.events.remove('change', this._onOptionsChange, this);
+            }
+        },
+
+        _tick: function (fromInterval) {
+            var options = this.options.getAll();
+
+            if (fromInterval &&
+                (Date.now() - (this._lastOptionsChangeTime)) < options.minUpdateInterval
+            ) {
+                return;
+            }
+
+            if (this._lastRequest) {
+                this._lastRequest.abort();
+            }
+
+            this._lastRequest = twitterDataProvider.getTweets(
+                options.twitterSearchQuery,
+                options.twitterGeocodeQuery,
+                this._onTweetsLoad,
+                {
+                    callbackCtx: this
+                }
+            );
+        },
+
+        _onTweetsLoad: function (tweets) {
+            this._lastRequest = null;
+            this.events.fire('tweetsloaded', {tweets: tweets});
+        },
+
+        _onOptionsChange: function () {
+            console.log('scheduler: options changed');
+            if (this.options.get('loadTweetsOnOptionsChanges')) {
+                this._lastOptionsChangeTime = Date.now();
+                this._tick();
+            }
+        }
+    });
+
     function getTwitterUserLink(username) {
         return ['<a href="http://www.twitter.com/', username,
             '" target="_blank">@', username, '</a>'].join('');
